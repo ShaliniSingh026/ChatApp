728d4a3ff8a08cd53642e33b3dcb8c67d1c365e3
ShaliniSingh026 <Shalinisk026@gmail.com>
2022-03-13 17:55:50 +0530

    Chat App

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const path = require('path');
+const express = require('express');
 const http = require('http');
-const express = require('express')
+const path = require('path');
+//const bodyparser = require('body-parser')
 const socketio = require('socket.io');
 const { formatMessage, generateLocationMessage} = require('./utils/messages');
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 const app = express()
 //const jQuery = require('jQuery');
+//app.use(bodyparser.urlencoded({extended:true}))
 const server = http.createServer(app);
 const io = socketio(server);
 //const publicPath = path.join(__dirname, 'public');
-const PORT = 3000 || process.env.port;
+const PORT = process.env.PORT || 4000;
 //set static folder
 //app.use(express.static(publicPath));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, './public')));
+app.get('',(req,res)=> {
+res.sendFile(__dirname + '/public/join.html')
+})
 
 const botName = 'Admin';
 
